372fd3299c4a06d9e65236173a30e004ca348b83
Eivind Sorteberg <eivind.sorteberg@gmail.com>
2020-02-25 09:06:46 +0100

    #2 Add Phaser framework

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
+import * as Phaser from "phaser";
+
 // index.ts
 console.log("Hello world");
+
+var config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  dom: {
+    createContainer: false
+  },
+  scene: null,
+  backgroundColor: 0x333333
+};
+var game = new Phaser.Game(config);
