66b0b0170029f59201cc13d3d78cb6bc943fe5f6
Eivind Sorteberg <eivind.sorteberg@gmail.com>
2020-02-25 14:38:38 +0100

    #12 Add collision detection

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const boardSize = {
 };
 const size = 10;
 
-var config = {
+var config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
@@ -27,9 +27,20 @@ var config = {
     create: create,
     update: update
   },
-  backgroundColor: 0x333333
+  backgroundColor: 0x333333,
+  physics: {
+    default: "arcade",
+    matter: {
+      debug: true,
+      gravity: false
+    },
+    arcade: {}
+  }
 };
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
+
+let history: Phaser.GameObjects.GameObject[] = [];
+let tail: Phaser.GameObjects.GameObject[] = [];
 
 let graphics: Phaser.GameObjects.Graphics;
 
@@ -38,18 +49,22 @@ let position: {
   y: number;
 };
 let angle: number;
-let speed = 3;
+let speed = 4;
 let keys: {
   left: Phaser.Input.Keyboard.Key;
   right: Phaser.Input.Keyboard.Key;
   down: Phaser.Input.Keyboard.Key;
   up: Phaser.Input.Keyboard.Key;
+  d: Phaser.Input.Keyboard.Key;
 };
+let head: Phaser.GameObjects.Rectangle;
 
 let path: Phaser.Curves.Path;
 
 function create(this: Phaser.Scene) {
   graphics = this.add.graphics();
+
+  graphics.lineStyle(size, 0x00ff00, 0.3);
 
   this.input.enabled = true;
   position = {
@@ -64,8 +79,13 @@ function create(this: Phaser.Scene) {
     left: this.input.keyboard.addKey("LEFT"),
     right: this.input.keyboard.addKey("RIGHT"),
     down: this.input.keyboard.addKey("DOWN"),
-    up: this.input.keyboard.addKey("UP")
+    up: this.input.keyboard.addKey("UP"),
+    d: this.input.keyboard.addKey("d")
   };
+
+  head = this.add.rectangle(position.x, position.y, size, size, 0xffffff);
+
+  this.physics.add.existing(head);
 }
 
 function update(this: Phaser.Scene) {
@@ -83,29 +103,37 @@ function update(this: Phaser.Scene) {
   if (keys.up.isDown) {
     speed += 1;
   }
+  if (keys.d.isDown) {
+    this.physics.world.createDebugGraphic();
+  }
 
   const dx = speed * Math.cos(angle);
   const dy = speed * Math.sin(angle);
 
-  position.x += dx;
-  position.y += dy;
+  head.x += dx;
+  head.y += dy;
+
+  history.push(this.add.rectangle(head.x, head.y, size, size));
+  if (history.length > 8) {
+    const crashable = history.shift();
+    tail.push(crashable);
+
+    this.physics.add.existing(crashable);
+    this.physics.add.collider(head, crashable, () => console.log("Yaaaay!!"));
+  }
 
   graphics.lineStyle(size, 0x00ff00, 0.3);
-  path.lineTo(position.x, position.y);
-
+  path.lineTo(head.x, head.y);
   path.draw(graphics);
 
   if (
-    position.x >= boardSize.width ||
-    position.x <= 0 ||
-    position.y >= boardSize.height ||
-    position.y <= 0
+    head.x >= boardSize.width ||
+    head.x <= 0 ||
+    head.y >= boardSize.height ||
+    head.y <= 0
   ) {
-    position.x -= dx;
-    position.y -= dy;
+    head.x -= dx;
+    head.y -= dy;
     speed = 0;
   }
-
-  graphics.fillStyle(0xff0000, 1);
-  graphics.fillCircle(position.x, position.y, size / 2);
 }
