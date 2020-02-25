e58b77c1cc25a8474a6e0cfb7b36a3d88ceffeb0
Eivind Sorteberg <eivind.sorteberg@gmail.com>
2020-02-25 10:24:33 +0100

    #4 Add keyboard input

--- src/index.ts
+++ src/index.ts
@@ -31,37 +31,64 @@ let position: {
   x: number;
   y: number;
 };
-let direction: {
-  dx: number;
-  dy: number;
+let angle: number;
+let speed = 5;
+let keys: {
+  left: Phaser.Input.Keyboard.Key;
+  right: Phaser.Input.Keyboard.Key;
+  down: Phaser.Input.Keyboard.Key;
+  up: Phaser.Input.Keyboard.Key;
 };
 
-function create() {
+function create(this: Phaser.Scene) {
   graphics = this.add.graphics();
 
+  this.input.enabled = true;
   position = {
     x: 100,
     y: 100
   };
-  direction = {
-    dx: 10,
-    dy: 5
+  angle = 0;
+
+  keys = {
+    left: this.input.keyboard.addKey("LEFT"),
+    right: this.input.keyboard.addKey("RIGHT"),
+    down: this.input.keyboard.addKey("DOWN"),
+    up: this.input.keyboard.addKey("UP")
   };
 }
 
 function update() {
   graphics.clear();
 
-  position.x += direction.dx;
-  position.y += direction.dy;
-
-  if (position.x >= boardSize.width || position.x <= 0) {
-    direction.dx *= -1;
-    position.x += direction.dx;
+  if (keys.left.isDown) {
+    angle -= ((((2 * Math.PI) / 1000) * speed) % 2) * Math.PI;
   }
-  if (position.y >= boardSize.height || position.y <= 0) {
-    direction.dy *= -1;
-    position.y += direction.dy;
+  if (keys.right.isDown) {
+    angle += ((((2 * Math.PI) / 1000) * speed) % 2) * Math.PI;
+  }
+  if (keys.down.isDown) {
+    speed -= 1;
+  }
+  if (keys.up.isDown) {
+    speed += 1;
+  }
+
+  const dx = speed * Math.cos(angle);
+  const dy = speed * Math.sin(angle);
+
+  position.x += dx;
+  position.y += dy;
+
+  if (
+    position.x >= boardSize.width ||
+    position.x <= 0 ||
+    position.y >= boardSize.height ||
+    position.y <= 0
+  ) {
+    position.x -= dx;
+    position.y -= dy;
+    speed = 0;
   }
 
   graphics.fillStyle(0xff0000, 1);
