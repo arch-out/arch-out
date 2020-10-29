eed4ff0f430082b2f6baae676486e4328c3244e3
ainthu <Aina.thunestveit@bekk.no>
2020-10-29 11:03:38 +0100

    Lagt til terningkast på title-screen

--- src/title-scene.ts
+++ src/title-scene.ts
@@ -1,5 +1,6 @@
 import GameScene from "./game-scene";
 import titlePath from "./images/gameTitle.png";
+import terningPath from "./images/terningkast6.png";
 import dagPath from "./images/dag.png";
 import ainaPath from "./images/aina.png";
 import eivindPath from "./images/eivind.png";
@@ -15,6 +16,7 @@ export default class TitleScene extends Phaser.Scene {
 
   preload() {
     this.load.image("title", titlePath);
+    this.load.image("terningkast6", terningPath);
     this.load.image("dag", dagPath);
     this.load.image("aina", ainaPath);
     this.load.image("eivind", eivindPath);
@@ -26,8 +28,33 @@ export default class TitleScene extends Phaser.Scene {
     var centerX = this.cameras.main.centerX;
     var centerY = this.cameras.main.centerY;
     
-    var titleImgage = this.add.image(centerX, centerY - 120, "title");
-    titleImgage.setScale(0.5);
+    var titleImage = this.add.image(centerX, centerY - 160, "title");
+    titleImage.setScale(0.5);
+
+    var text = this.add.text(centerX, centerY - 90, "START").setOrigin(0.5);
+    text.setInteractive({ useHandCursor: true });
+    text.on("pointerdown", () => this.startGame());
+
+    this.add
+      .text(centerX, centerY - 60, "Press 'F' for full screen.", {
+        fontSize: 10,
+      })
+      .setOrigin(0.5);
+
+    this.add
+      .text(centerX, centerY + 10, "Det besta spelet eg nokon gong har spelt", {
+        fontSize: 14,
+      })
+      .setOrigin(0.5);
+
+    var terningImage = this.add.image(centerX + 200, centerY + 10, "terningkast6");
+    terningImage.setScale(0.2);
+
+    this.add
+      .text(centerX + 70, centerY + 30, "- sa ingen", {
+        fontSize: 10,
+      })
+      .setOrigin(0.5);
 
     var dag = this.add
       .image(centerX - 200, centerY + 180, "dag")
@@ -39,19 +66,9 @@ export default class TitleScene extends Phaser.Scene {
       .setScale(0.75);
     eivind.rotation = -0.3;
 
-    var text = this.add.text(centerX, centerY - 50, "START").setOrigin(0.5);
-    text.setInteractive({ useHandCursor: true });
-    text.on("pointerdown", () => this.startGame());
-
-    this.add
-      .text(centerX, centerY - 20, "Press 'F' for full screen.", {
-        fontSize: 10,
-      })
-      .setOrigin(0.5);
-
     this.add
       .text(centerX, centerY + 100, "Stolt presentert av:", {
-        fontSize: 14,
+        fontSize: 16,
       })
       .setOrigin(0.5);
 
